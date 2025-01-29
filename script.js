3b91e983466e304c56c1beaba60cd257c2782ffb
MideJoy Akinleye <joyintech50@gmail.com>
2025-01-29 16:05:56 +0100

    update on quiz

--- script.js
+++ script.js
@@ -1,65 +1,65 @@
 const questions = [
     {
-        Question: "Pick the rught property of a css Display?",
+        question: "Pick the right property of a CSS Display?",
         answer: [
-        {text: "Flex", correct: true},
-        {text: "Arial-shadow", correct: false},
-        {text: "display", correct: false},
-        {text: "Content-line", correct: false},
-        {text: "color", correct: false}
+            {text: "Flex", correct: true},
+            {text: "Arial-shadow", correct: false},
+            {text: "display", correct: false},
+            {text: "Content-line", correct: false},
+            {text: "color", correct: false},
         ]
     },
     {
-        Question: "What tag is used in breaking line/sentences in HTML",
+        question: "What tag is used in breaking lines/sentences in HTML?",
         answer: [
-        {text: "<em>", correct: true},
-        {text: "<li>", correct: false},
-        {text: "<br>", correct: false},
-        {text: "<a>", correct: false},
-        {text: "color", correct: false}
+            {text: "em", correct: false},
+            {text: "li", correct: false},
+            {text: "br", correct: true},
+            {text: "a", correct: false},
+            {text: "color", correct: false},
         ]
     },
     {
-        Question: "Pick the right re-usable variable",
+        question: "Pick the right re-usable variable?",
         answer: [
-        {text: "selector", correct: true},
-        {text: "array", correct: false},
-        {text: "const", correct: false},
-        {text: "const-line", correct: false},
-        {text: "let", correct: false}
+            {text: "selector", correct: false},
+            {text: "array", correct: false},
+            {text: "const", correct: false},
+            {text: "const-line", correct: false},
+            {text: "let", correct: true},
         ]
     },
     {
-        Question: "What do you understand by (CSS)",
+        question: "What do you understand by (CSS)?",
         answer: [
-        {text: "For styling", correct: true},
-        {text: "For structure", correct: false},
-        {text: "For luck", correct: false},
-        {text: "Fun-line", correct: false},
-        {text: "Enjoyment", correct: false}
+            {text: "For styling", correct: true},
+            {text: "For structure", correct: false},
+            {text: "For luck", correct: false},
+            {text: "Fun-line", correct: false},
+            {text: "Enjoyment", correct: false},
         ]
     },
     {
-        Question: "Pick the rught property of a css Display?",
+        question: "Pick the right property of a CSS Display?",
         answer: [
-        {text: "Flex", correct: true},
-        {text: "Arial-shadow", correct: false},
-        {text: "display", correct: false},
-        {text: "Content-line", correct: false},
-        {text: "color", correct: false}
+            {text: "Flex", correct: true},
+            {text: "Arial-shadow", correct: false},
+            {text: "display", correct: false},
+            {text: "Content-line", correct: false},
+            {text: "color", correct: false},
         ]
     },
     {
-        Question: "Full meaning of FTP",
+        question: "Full meaning of FTP?",
         answer: [
-        {text: "File trophy protocol", correct: true},
-        {text: "File transferring protocol", correct: false},
-        {text: "File teleporting protocol", correct: false},
-        {text: "File-line transfer protocol", correct: false},
-        {text: "File transfer protocol", correct: false}
+            {text: "File Trophy protocol", correct: false},
+            {text: "File transfering protocol", correct: false},
+            {text: "File teleporting protocol", correct: false},
+            {text: "File-line transfer protocol", correct: false},
+            {text: "File transfer protocol", correct: true},
         ]
     }
-]
+];
 
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
@@ -68,9 +68,81 @@ const nextButton = document.getElementById("next-btn");
 let currentQuestionIndex = 0;
 let score = 0;
 
-const startQuiz = () => {
- currentQuestionIndex = 0;
- score = 0;
- nextButton.innerHTML = 'Next';
- showQuestion();
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
 }
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
+
+    currentQuestion.answer.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}! 🥳🎊🎉`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+
+startQuiz();
